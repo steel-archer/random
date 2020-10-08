e8a3e257fdb01e1da285078e582170c5ed4cbfc1
Bohdan Sokulskyi <bsokulskyi@n-ix.com>
2020-10-09 00:24:28 +0300

    App seems to be ready!

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # random
-Visualization of different PHP random functions
+An attempt to check whether different PHP randomization functions demonstrate noticeably different distributions (spoiler: not too much).
+
+Image generation code has been taken here: https://boallen.com/random-numbers.html
+
+All you need to run this app is **docker** and **docker-compose**.
+
+How to set up this app:
+* Clone this repo: `git clone git@github.com:steel-archer/random.git`
+* Go to the cloned directory: `cd random`
+* Run docker: `docker-compose up -d`
+* You can check how it works here: http://localhost:8008/
+* Shut down docker after you finish: `docker-compose down --remove-orphans`
+
+Used:
+* PHP 7.4
+* GD extension
---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This function code is mostly take from here
+ *
+ * @param string $funcName
+ * @param callable|null $seedFunc
+ * @param callable $genFunc
+ */
+function draw(string $funcName, ?callable $seedFunc, callable $genFunc): void
+{
+    if (is_callable($seedFunc)) {
+        $seedFunc();
+    }
+
+    $length = 512;
+    $im = imagecreatetruecolor($length, $length) or die('Cannot Initialize new GD image stream');
+    $white = imagecolorallocate($im, 255, 255, 255);
+
+    for ($y = 0; $y < $length; $y++) {
+        for ($x = 0; $x < $length; $x++) {
+            if ($genFunc()) {
+                imagesetpixel($im, $x, $y, $white);
+            }
+        }
+    }
+
+    ob_start();
+    imagepng($im);
+    imagedestroy($im);
+    $image = base64_encode(ob_get_clean());
+
+    $output = <<<OUTPUT
+<b>{$funcName}:</b>
+<br>
+<img src='data:image/png;base64,{$image}' />
+<br><br>
+OUTPUT;
+    echo $output;
+}
+
+$allFuncs = [
+    'mt_rand' => [
+        'seedFunc' => static function () {
+            mt_srand(0, MT_RAND_MT19937);
+        },
+        'genFunc' => static function () {
+            return mt_rand(0, 1);
+        },
+    ],
+    'mt_rand (old algorithm)' => [
+        'seedFunc' => static function () {
+            mt_srand(0, MT_RAND_PHP);
+        },
+        'genFunc' => static function () {
+            return mt_rand(0, 1);
+        },
+    ],
+    'random_int' =>  [
+        'seedFunc' => null,
+        'genFunc' => static function () {
+            return random_int(0, 1);
+        },
+    ],
+    'random_bytes' => [
+        'seedFunc' => null,
+        'genFunc' => static function () {
+            return current(unpack('n', random_bytes(2))) % 2;
+        }
+    ],
+    'openssl_random_pseudo_bytes' => [
+        'seedFunc' => null,
+        'genFunc' => static function () {
+            return current(unpack('n', openssl_random_pseudo_bytes(2))) % 2;
+        }
+    ],
+    'uniqid' => [
+        'seedFunc' => null,
+        'genFunc' => static function () {
+            return hexdec(uniqid('', false)) % 2;
+        }
+    ],
+    'uniqid (more entropy)' => [
+        'seedFunc' => null,
+        'genFunc' => static function () {
+            $string = uniqid('', true); // E.g. 5f7f8002e1f033.79832804
+            $parts = explode('.', $string);
+            return (hexdec($parts[0]) + $parts[1]) % 2;
+        }
+    ],
+];
+
+foreach ($allFuncs as $funcName => $funcs) {
+    draw($funcName, $funcs['seedFunc'], $funcs['genFunc']);
+}
